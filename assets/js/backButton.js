49a6a335a24c63d1bf1672777c8359b9ee719508
MOJ <43385016+mojcodes@users.noreply.github.com>
2024-06-08 13:22:25 +0100

    Delete assets directory

--- assets/js/backButton.js
+++
@@ -1,9 +0,0 @@
-export default function BackButtonInit() {
-    const backButton = document.getElementById("backButton");
-    if (backButton) {
-        backButton.addEventListener("click", () => {
-            if (window.history.length > 1) window.history.back();
-            else window.location.href = "/";
-        });
-    }
-}
--- assets/js/notifications.js
+++
@@ -1,8 +0,0 @@
-import { clear_notification_counter } from "./utils/notificationCounter";
-
-(() => {
-    clear_notification_counter();
-    if (Notification.permission === "default") {
-        Notification.requestPermission();
-    }
-})();
